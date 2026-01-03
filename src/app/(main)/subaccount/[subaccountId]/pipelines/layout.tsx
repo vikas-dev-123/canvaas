443a2e8ac4081c14401a1b5a08241d3db018b0a9
Vikas Dev Pandey <vikaspandey0234@gmail.com>
2026-01-03 22:17:49 +0530

    setting up the payment integration part funcionality with all free plan and paid plan logic

--- src/app/(main)/subaccount/[subaccountId]/pipelines/layout.tsx
+++ src/app/(main)/subaccount/[subaccountId]/pipelines/layout.tsx
@@ -3,7 +3,7 @@ import BlurPage from "@/components/global/blur-page";
 import React from "react";
 
 const Layout = ({ children }: ChildrenProps) => {
-    return <BlurPage>{children}</BlurPage>;
+    return <BlurPage>{children}</BlurPage>;//children will be the Page component
 };
 
 export default Layout;
