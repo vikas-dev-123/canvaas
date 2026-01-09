21cbe0244a7d02f362dfc5ef4ce5a6a02440f237
Vikas Dev Pandey <vikaspandey0234@gmail.com>
2026-01-09 22:34:56 +0530

    add comment to clarify loading screen for agency page

--- src/app/(main)/agency/loading.tsx
+++ src/app/(main)/agency/loading.tsx
@@ -2,7 +2,7 @@ import Loading from "@/components/global/loading";
 import React from "react";
 
 const LoadingAgencyPage = () => {
-    return (
+    return ( //loading screen for agency page
         <div className="h-screen w-screen flex justify-center items-center">
             <Loading></Loading>
         </div>
