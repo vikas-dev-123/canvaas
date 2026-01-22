b259934b601a52b9695a8360b2789c7eda1674cc
Vikas Dev Pandey <vikaspandey0234@gmail.com>
2026-01-22 22:34:26 +0530

    make the login page in center

--- app/(main)/agency/(auth)/layout.tsx
+++ app/(main)/agency/(auth)/layout.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
-    return <div className="h-full items-center justify-center">{children}</div>;
+    return <div className="min-h-screen flex items-center justify-center">{children}</div>;
 };
 
 export default AuthLayout;
